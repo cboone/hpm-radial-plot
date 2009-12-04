66151c7c1c0a07d2448ebf6ce6327a833e4416ed
Christopher Boone <chris@hypsometry.com>
2009-12-04 14:27:22 -0800

    Don't need no stinking boxed labels.

--- R/radial.plot.R
+++ R/radial.plot.R
@@ -1,33 +1,3 @@
-boxed.labels<-function(x,y=NA,labels,
- bg=ifelse(match(par("bg"),"transparent",0),"white",par("bg")),
- border=TRUE,xpad=1.2,ypad=1.2,srt=0,cex=1,adj=0.5,...) {
- 
- oldcex<-par("cex")
- par(cex=cex)
- if(is.na(y) && is.list(x)) {
-  y<-unlist(x[[2]])
-  x<-unlist(x[[1]])
- }
- box.adj<-adj+(xpad-1)*(0.5-adj)
- if(srt == 90 || srt == 270) {
-  bheights<-strwidth(labels)
-  theights<-bheights*(1-box.adj)
-  bheights<-bheights*box.adj
-  lwidths<-rwidths<-strheight(labels)*0.5
- }
- else {
-  lwidths<-strwidth(labels)
-  rwidths<-lwidths*(1-box.adj)
-  lwidths<-lwidths*box.adj
-  bheights<-theights<-strheight(labels)*0.5
- }
- rect(x-lwidths*xpad,y-bheights*ypad,x+rwidths*xpad,
-  y+theights*ypad,col=bg,border=border)
- text(x,y,labels,srt=srt,cex=cex,adj=adj,...)
- par(cex=oldcex)
-}
-
-
 # TODO: Straighten out the grid displaying situation. Allow for printing range labels other than horizontally.
 # 
 hpm.radial.plot <- function(data.values, data.angles = NULL, plot.type = "p",
