fe331aac80dcc4c71d053838558584c859192d40
Christopher Boone <chris@hypsometry.com>
2009-12-04 13:33:01 -0800

    Fixing up the exact range plotting.

--- R/test.R
+++ R/test.R
@@ -3,5 +3,5 @@
 source("R/radial.plot.R")
 
 # hpm.radial.plot(1:365)
-# hpm.radial.plot(3:11, exact.range = FALSE)
-hpm.radial.plot(3:11)
+hpm.radial.plot(3:11, exact.range = FALSE)
+# hpm.radial.plot(3:11)
